a3699e5e409ca192eb10c245b880f405c425e981
vincenttesla <vencent.tesla@gmail.com>
2016-04-14 19:09:10 +0800

    vis alpha

--- js/vis.js
+++ js/vis.js
@@ -1,15 +1,51 @@
 //数据对象
-var data = [];
+var echartsData = "";
 //dom元素
 var dataInput = $("#data");
-var dataBtn = $("#btn");
+var dataBtn = $("#btn_data");
+var ajaxInput = $("#ajax");
+var ajaxBtn = $("#btn_ajax");
+var ajaxUrl = "";
+var optBtn = $("#btn_opt");
+var titleName = $("#title_name");
+var xName = $("#x_name");
+var yName = $("#y_name");
+var dataZoomOp = $("#data_zoom");
+var toolboxOp = $("#toolbox");
+var serLineOp = $("#ser_line");
 var chart = echarts.init(document.getElementById('chart'));
 //option初始化
 var option = {
             title: {
-                text: 'ECharts 入门示例'
+                text: ''
             },
             tooltip: {},
+            toolbox: {
+			show: false,
+			feature: {
+				mark: {
+					show: false
+				},
+				dataView: {
+					show: true,
+					readOnly: false
+				},
+				magicType: {
+					show: true,
+					type: ['line', 'bar']
+				},
+				restore: {
+					show: true
+				},
+				saveAsImage: {
+					show: true
+				}
+			}
+		},
+		dataZoom: {
+			show: false,
+			start: 0,
+		},
             legend: {
                 data:[]
             },
@@ -18,18 +54,65 @@ var option = {
             },
             yAxis: {},
             series: [{
-                name: '销量',
-                type: 'bar',
-                data: []
+                name: '',
+                type: 'line',
+                data: [],
+                markPoint: {
+				data: [{
+					type: 'max',
+					name: '最大值'
+				}, {
+						type: 'min',
+						name: '最小值'
+					}]
+			},
+			markLine: {
+				data: [{
+					type: 'average',
+					name: '平均值'
+				}]
+			}
             }]
         };
 //输入数据
 function addData() {
 	if(!inputIsNull(dataInput)){
-		data = $.parseJSON(dataInput.val())[0]["data"];
+		echartsData = $.parseJSON(dataInput.val())[0]["data"];
 		setEchartsOp();
 		initEcharts();
 	}
+}
+//输入ajax请求
+function addAjax(){
+	if(!inputIsNull(ajaxInput)){
+		if(isAjaxUrl(ajaxInput)){
+			chart.showLoading();
+			doAjax();			
+		}
+	}
+}
+//增加高级配置
+function addSuperOption(){
+	option.title.text = $.trim(titleName.val());
+	option.xAxis.name = $.trim(xName.val());
+	option.yAxis.name = $.trim(yName.val());
+	option.dataZoom.show = dataZoomOp.is(":checked");
+	option.toolbox.show = toolboxOp.is(":checked");
+	if(serLineOp.is(":checked")){
+		option.series[0].type = 'line';
+	}else{
+		option.series[0].type = 'bar';
+	}
+	initEcharts();
+}
+//进行ajax请求
+function doAjax(){
+	$.get(ajaxUrl).done(function (data) {
+	chart.hideLoading();
+    echartsData = $.parseJSON(data)[0]["data"];
+	setEchartsOp();
+	initEcharts();
+});
 }
 //设置Echarts的Option
 function setEchartsOp(){
@@ -40,17 +123,14 @@ function setEchartsOp(){
 function handelData(){
    var x = [];
     var y = [];
-    var j = 0;
-    var size = data.length;
+    var size = echartsData.length;
     for(var i = 0;i < size;i ++){
-        for(key in data[i]){
-            if(j == 0)
-                y[i] = parseInt(data[size - i - 1][key]);
-            else                
-                x[i] = data[size - i - 1][key];
-            j++;
+        for(key in echartsData[i]){
+            if(isADate(echartsData[size - i - 1][key]))
+            	x[i] = echartsData[size - i - 1][key];
+            else             
+                y[i] = parseInt(echartsData[size - i - 1][key]);
         }
-        j = 0;
     }
     return {
         time : x,
@@ -63,7 +143,7 @@ function initEcharts(){
 }
 //判断表单是否为空
 function inputIsNull(position){
-	inp = $.trim(position.val());
+	var inp = $.trim(position.val());
 	if(inp == null || inp == ""){
 		position.next().show();
 		return true;
@@ -73,7 +153,25 @@ function inputIsNull(position){
 	}
 	
 }
+//判断url是否合法
+function isAjaxUrl(position){
+	ajaxUrl = $.trim(position.val());
+	var exp = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+	return exp.test(ajaxUrl);
+}
+//判断是否是日期格式(yy-mm-dd)
+function isADate(date){
+	var exp1 = /^(?:(?!0000)[0-9]{4}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)-02-29)$/;
+	var exp2 = /^(?:(?!0000)[0-9]{4}(?:(?:0[1-9]|1[0-2])(?:0[1-9]|1[0-9]|2[0-8])|(?:0[13-9]|1[0-2])(?:29|30)|(?:0[13578]|1[02])31)|(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)0229)$/;
+	return exp1.test(date)||exp2.test(date);
+}	
 //绑定事件
 dataBtn.on("click",function(){
 	addData();
+});
+ajaxBtn.on("click",function(){
+	addAjax();
+});
+optBtn.on("click",function(){
+	addSuperOption();
 });
