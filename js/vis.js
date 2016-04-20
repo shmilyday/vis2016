c6329fd3e449360eb89bbd33afc451fa15fde234
vincenttesla <vencent.tesla@gmail.com>
2016-04-20 17:13:43 +0800

    modified vis.js

--- js/vis.js
+++ js/vis.js
@@ -6,6 +6,11 @@ var dataBtn = $("#btn_data");
 var ajaxInput = $("#ajax");
 var ajaxBtn = $("#btn_ajax");
 var ajaxUrl = "";
+var xyBtn = $("#btn_xy");
+var xWraper = $("#wraper_x");
+var yWraper = $("#wraper_y");
+var fileInput = $("#local_file");
+var fileBtn = $("#btn_file");
 var optBtn = $("#btn_opt");
 var titleName = $("#title_name");
 var xName = $("#x_name");
@@ -78,8 +83,7 @@ var option = {
 function addData() {
 	if(!inputIsNull(dataInput)){
 		echartsData = $.parseJSON(dataInput.val())[0]["data"];
-		setEchartsOp();
-		initEcharts();
+		renderXY();
 	}
 }
 //输入ajax请求
@@ -89,6 +93,29 @@ function addAjax(){
 			chart.showLoading();
 			doAjax();			
 		}
+	}
+}
+//绘制xy轴可选项
+function renderXY(){
+	var htmlx = "";
+	var htmly = "";
+	for(key in echartsData[0]) {
+		htmlx += "<input type=\"radio\" name=\"x_radio\" id=\"x_"+key+"\" value=\""+key+"\"><label for=\"x_"+key+"\">"+key+"</label>";
+		htmly += "<input type=\"radio\" name=\"y_radio\" id=\"y_"+key+"\" value=\""+key+"\"><label for=\"y_"+key+"\">"+key+"</label>";
+	}
+	xWraper.html(htmlx);
+	yWraper.html(htmly);
+}
+//选择xy，并绘制
+function chooseXY(){
+	var checkedX = $("#wraper_x input:checked").val();
+	var checkedY = $("#wraper_y input:checked").val();
+	if(checkedX != checkedY){
+		$("#xy_cution").hide();
+		setEchartsOp(checkedX,checkedY);
+		initEcharts();
+	}else{
+		$("#xy_cution").show();
 	}
 }
 //增加高级配置
@@ -110,31 +137,32 @@ function doAjax(){
 	$.get(ajaxUrl).done(function (data) {
 	chart.hideLoading();
     echartsData = $.parseJSON(data)[0]["data"];
-	setEchartsOp();
-	initEcharts();
+	renderXY();
 });
 }
 //设置Echarts的Option
-function setEchartsOp(){
-	option.xAxis.data = handelData().time;
-	option.series[0].data = handelData().data;
+function setEchartsOp(x,y){
+	option.xAxis.data = handelData(x,y).x;
+	option.xAxis.name = x;
+	option.series[0].data = handelData(x,y).y;
+	option.yAxis.name = y;
 }
 //将json对象里的数据分离提取
-function handelData(){
+function handelData(x1,y1){
    var x = [];
     var y = [];
     var size = echartsData.length;
     for(var i = 0;i < size;i ++){
         for(key in echartsData[i]){
-            if(isADate(echartsData[size - i - 1][key]))
+            if(key == x1)
             	x[i] = echartsData[size - i - 1][key];
-            else             
-                y[i] = parseInt(echartsData[size - i - 1][key]);
+            if(key == y1)             
+                y[i] = echartsData[size - i - 1][key];
         }
     }
     return {
-        time : x,
-        data : y
+        x : x,
+        y : y
     }
 }
 //初始化chart
@@ -164,6 +192,17 @@ function isADate(date){
 	var exp1 = /^(?:(?!0000)[0-9]{4}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)-02-29)$/;
 	var exp2 = /^(?:(?!0000)[0-9]{4}(?:(?:0[1-9]|1[0-2])(?:0[1-9]|1[0-9]|2[0-8])|(?:0[13-9]|1[0-2])(?:29|30)|(?:0[13578]|1[02])31)|(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)0229)$/;
 	return exp1.test(date)||exp2.test(date);
+}
+//判断文件是否是excel文件
+function  isExcelFile(){
+	var fContent = fileInput.val();
+	var fileext = fContent.substring(fContent.lastIndexOf("."),fContent.length)  
+    fileext = fileext.toLowerCase()  
+    if (fileext!='.xls'){
+    	return true;
+    }else{
+    	return false;
+    }
 }	
 //绑定事件
 dataBtn.on("click",function(){
@@ -172,6 +211,12 @@ dataBtn.on("click",function(){
 ajaxBtn.on("click",function(){
 	addAjax();
 });
+fileBtn.on("click",function(){
+	readLocalFile();
+})
 optBtn.on("click",function(){
 	addSuperOption();
 });
+xyBtn.on("click",function(){
+	chooseXY();
+})
